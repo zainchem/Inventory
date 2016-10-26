feea150a75a0fa6fc85fbd22baebb2ad44ccea60
zainchem <nik43581@gmail.com>
2016-10-25 17:20:18 -0700

    https://github.com/zainchem/Inventory.git

---
+++ PStest/Models/InventoryItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PStest.Models
+{
+    public class InventoryItem
+    {
+        public string Model { get; set; }
+        public string Description {get; set;}
+        public int MinQuantity {get; set;}
+        public string PartNumber { get; set; }
+
+    }
+}
